5f574464085877b6ebd3d3485e4ab5297eec9974
christian-westbrook <christian.a.westbrook@proton.me>
2025-12-01 20:43:15 -0600

    feat: Functioning example of Candela selecting the next film and presenting the User with messaging that simulates an Advent calendar

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Candela
 
-An advent calendar for Christmas movies!
+When you're busy during the holidays, choosing the next Christmas movie to watch can be hard. Candela puts a fun twist on this decision by creating an Advent calendar of uplifting Christmas films for you to open each day leading up to Christmas.
+
+## Dependencies
+
+Candela temporarily requires Ruby to be installed on your machine. Stay tuned for an update that will allow you to run Candela without installing anything extra or using the command-line.
 
 ## Usage
 
-Create a file called `films.txt` in the same directory as `candela.rb`. Each line of `films.txt` should contain the title of a film that you would like to be a part of your advent calendar.
+Create a file called `films.txt` and place it in the same directory as `candela.rb`. Each line of `films.txt` should contain the title of a film that you would like to be a part of your calendar. When you're ready to open the first film in your calendar, run Candela with `ruby candela.rb`. Run Candela again each day to open another film to watch. Merry Christmas!
--- candela.rb
+++ candela.rb
@@ -1,8 +1,82 @@
 #!/usr/bin/env ruby
 
-def ingest_films
-    file_of_films = File.new("films.txt")
-    file_of_films.readlines
+require 'json'
+
+class Calendar
+    attr_accessor :sealed_films
+    attr_accessor :opened_films
+
+    def initialize()
+        @sealed_films = []
+        @opened_films = []
+
+        @sealed_file_path = "sealed.txt"
+        @opened_file_path = "opened.txt"
+
+        if not File.exist?(@sealed_file_path)
+            sealed_file = File.new(@sealed_file_path, "w")
+            @sealed_films = ingest_films
+            write_films
+        end
+    end
+        
+    def ingest_films
+        file_of_films = File.new("films.txt")
+        file_of_films.readlines.map { |line| line.strip }
+    end
+    
+    def open_a_film()
+        sealed_films_json = File.read(@sealed_file_path)
+        opened_films_json = File.read(@opened_file_path)
+
+        @sealed_films = JSON.parse(sealed_films_json)
+        @opened_films = JSON.parse(opened_films_json)
+
+        sealed_films_left = @sealed_films.length
+        random_film_index = rand(0..(sealed_films_left - 1))
+        random_sealed_film = @sealed_films[random_film_index]
+
+        @opened_films.push(random_sealed_film)
+        @sealed_films.delete_at(random_film_index)
+
+        write_films
+
+        print "\nIt's day #{(@opened_films.length)} of your Advent calendar!"
+        sleep(1.0)
+        print " Only #{(25 - @opened_films.length)} days left until Christmas!"
+        sleep(1.0)
+        print "\n\n"
+        print "You peel open the next space on your Advent calendar and find "
+        sleep(1)
+        print "."
+        sleep(0.5)
+        print "."
+        sleep(0.5)
+        print "."
+        sleep(2)
+        print " #{random_sealed_film}!"
+        sleep(1)
+        print " Merry Christmas!"
+        sleep(2.0)
+        print "\n\n"
+        print "(Press Enter to exit) "
+        gets
+    end
+
+    def write_films
+        sealed_file = File.new(@sealed_file_path, "w")
+        opened_file = File.new(@opened_file_path, "w")
+
+        sealed_films_json = JSON.pretty_generate(@sealed_films)
+        opened_films_json = JSON.pretty_generate(@opened_films)
+
+        sealed_file.write(sealed_films_json)
+        opened_file.write(opened_films_json)
+
+        sealed_file.flush
+        opened_file.flush
+    end
 end
 
-puts ingest_films
+calendar = Calendar.new()
+calendar.open_a_film
